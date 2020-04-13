76d14f34bb4a6b1302c624520db1f502815b943f
박소영 <sysister103@gmail.com>
2020-04-13 17:33:35 +0900

    둘러보기 선택바 구현

---
+++ app/src/main/java/com/example/naeilo_project/CommunityActivity.java
@@ -0,0 +1,14 @@
+package com.example.naeilo_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CommunityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_community);
+    }
+}
