4ded8d75d40bdbf957a03e31e1256f115fdd4fc5
박소영 <sysister103@gmail.com>
2020-04-14 02:04:21 +0900

    activity_user_info.xml 내정보 구현

---
+++ app/src/main/java/com/example/naeilo_project/userInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.naeilo_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+}
