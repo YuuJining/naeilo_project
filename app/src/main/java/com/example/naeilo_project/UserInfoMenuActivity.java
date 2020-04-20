5535450a18834bb5d25e5dd75a7aa02e4c4ad87d
박소영 <sysister103@gmail.com>
2020-04-20 18:24:27 +0900

    둘러보기 star ratingBar로 레이아웃 수정, activity_user_info 설정 클릭시 메뉴 구현

---
+++ app/src/main/java/com/example/naeilo_project/UserInfoMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.naeilo_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserInfoMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info_menu);
+    }
+}
