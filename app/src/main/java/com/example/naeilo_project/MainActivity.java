dee5b967a787f3f0f88ecd3cd39843d9512d9926
Yujin Lee <yujin092444@gmail.com>
2020-05-12 18:11:54 +0900

    HomeFragement 구현

--- app/src/main/java/com/example/naeilo_project/MainActivity.java
+++ app/src/main/java/com/example/naeilo_project/MainActivity.java
@@ -19,23 +19,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bottomNavigationView=findViewById(R.id.bottom_navigation);
-        mainViewpager=findViewById(R.id.man_viewpager);
+        mainViewpager=findViewById(R.id.main_viewpager);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()) {
                     case R.id.home: {
+                        mainViewpager.setCurrentItem(0);
                         return true;
                     }
                     case R.id.community: {
+                        mainViewpager.setCurrentItem(1);
                         return true;
                     }
                     case R.id.userInfo: {
+                        mainViewpager.setCurrentItem(2);
                         return true;
                     }
+                    default:
+                        return false;
                 }
-                return true;
             }
         });
     }
