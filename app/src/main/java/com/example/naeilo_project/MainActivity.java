e8e4649ce4c2d9d08c1259aa82c1465695b10449
Yujin Lee <yujin092444@gmail.com>
2020-05-12 17:40:30 +0900

    mainviewpager 어댑터 구현 전까지

--- app/src/main/java/com/example/naeilo_project/MainActivity.java
+++ app/src/main/java/com/example/naeilo_project/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.naeilo_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
+    ViewPager2 mainViewpager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView=findViewById(R.id.bottom_navigation);
+        mainViewpager=findViewById(R.id.man_viewpager);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.home: {
+                        return true;
+                    }
+                    case R.id.community: {
+                        return true;
+                    }
+                    case R.id.userInfo: {
+                        return true;
+                    }
+                }
+                return true;
+            }
+        });
     }
 }
