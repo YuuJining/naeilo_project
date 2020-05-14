6fcb3426fac1d4d74b15db7282a101e76f59c6f5
박소영 <sysister103@gmail.com>
2020-05-15 01:41:38 +0900

    UserInfo 설정버튼 클릭시 이벤트-수정 중

--- app/src/main/java/com/example/naeilo_project/UserInfoMenuActivity.java
+++ app/src/main/java/com/example/naeilo_project/UserInfoMenuActivity.java
@@ -3,12 +3,14 @@ package com.example.naeilo_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class UserInfoMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_info_menu);
+        setContentView(R.layout.fragment_user_info_menu);
     }
 }
--- app/src/main/java/com/example/naeilo_project/UserInfoMenuFragment.java
+++ app/src/main/java/com/example/naeilo_project/UserInfoMenuFragment.java
@@ -5,26 +5,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class UserInfoFragment extends Fragment {
+public class UserInfoMenuFragment extends Fragment {
     ViewGroup viewGroup;
-    ImageButton environmentimg;
+    public static UserInfoMenuFragment newInstance() {
+        return new UserInfoMenuFragment();
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_user_info,container,false);
-
-        environmentimg = viewGroup.findViewById(R.id.environmentimg);
-        environmentimg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ((MainActivity)getActivity()).replaceFragment(UserInfoMenuFragment.newInstance());
-            }
-        });
+        viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_user_info_menu, container, false);
         return viewGroup;
     }
 }
