18ad3e9b4f2eae00bcadaca5299342a0b461579a
Yujin Lee <yujin092444@gmail.com>
2020-05-25 23:18:38 +0900

    Re_commit

--- app/src/main/java/com/example/naeilo_project/LoginActivity.java
+++ app/src/main/java/com/example/naeilo_project/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.naeilo_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -16,5 +19,11 @@ public class LoginActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().add(R.id.login_container,loginIntroFragment).commitAllowingStateLoss();
 
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.login_container, fragment).commit();
     }
 }
--- app/src/main/java/com/example/naeilo_project/SignUpFragment.java
+++ app/src/main/java/com/example/naeilo_project/SignUpFragment.java
@@ -7,49 +7,52 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class LoginIntroFragment extends Fragment {
+public class SignUpFragment extends Fragment {
 
     ViewGroup rootview;
-    Button sign_up;
-    Button kakao_login;
-    Button login;
+    Button sign_up_ok_btn;
+    Button sign_up_cancle_btn;
     Context mycontext;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        rootview = (ViewGroup) inflater.inflate(R.layout.fragment_login_intro,container,false);
+        rootview = (ViewGroup) inflater.inflate(R.layout.fragment_sign_up,container,false);
 
-        sign_up = rootview.findViewById(R.id.sign_up_btn);
-        login = rootview.findViewById(R.id.login_btn);
-        kakao_login = rootview.findViewById(R.id.kakao_login_btn);
+        sign_up_ok_btn = rootview.findViewById(R.id.sign_up_ok_btn);
+        sign_up_cancle_btn = rootview.findViewById(R.id.sign_up_cancel_btn);
 
-        login.setOnClickListener(new View.OnClickListener() {
+        sign_up_cancle_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(mycontext,MainActivity.class);
-                startActivity(intent);
+                ((LoginActivity)getActivity()).replaceFragment(new LoginIntroFragment());
             }
         });
 
-        sign_up.setOnClickListener(new View.OnClickListener() {
+        sign_up_ok_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ((LoginActivity)getActivity()).replaceFragment(new SignUpFragment());
+                Toast.makeText(mycontext,"회원가입이 완료되었습니다!", Toast.LENGTH_SHORT).show();
+
+                ((LoginActivity)getActivity()).replaceFragment(new LoginIntroFragment());
+
             }
         });
+
 
         return rootview;
     }
 
     @Override
     public void onAttach(@NonNull Context context) {
-        mycontext = context;
         super.onAttach(context);
+        mycontext = context;
+
     }
 }
